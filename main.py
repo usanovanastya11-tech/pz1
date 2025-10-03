bd0cf1383143bddc2626ffe5f7a2458ee1783baf
usanovanastya11-tech <usanovanastya11@gmail.com>
2025-10-03 19:15:34 +0300

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-print("Hello from main.py - modified by person1 in dev branch - DIFFERENT TEXT!")
+print("Hello from main.py - merged version after conflict resolution")
 def new_function():
     return "This is a new function"
-
-print("Additional line added by person2 in dev2 branch")
