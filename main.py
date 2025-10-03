54eaae3081011f707c5b81e6ad3861613321caf6
Anastasia Usanova <usanovanastya11@gmail.com>
2025-10-03 17:40:10 +0300

    Change first line in main.py with different text (person1 dev branch)

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print("Hello from main.py!")
+print("Hello from main.py - modified by person1 in dev branch - DIFFERENT TEXT!")
+def new_function():
+    return "This is a new function"
+
+print("Additional line added by person2 in dev2 branch")
