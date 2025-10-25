6889973f7e5ac3df95fce0112b2a34b1e21ef5f7
usanovanastya11-tech <usanovanastya11@gmail.com>
2025-10-25 18:49:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ print("Hello from main.py - merged version after conflict resolution")
 def new_function():
     return "This is a new function"
 
-print("Hello from main.py!")
+print("Hell from main.py!")
 
 from helper import greet, farewell
 
